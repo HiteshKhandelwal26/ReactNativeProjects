12baf40ca546f10606cf59c689777eeb529763d0
Hitesh Khandelwal <hiteshkhandelwal@Hiteshs-MacBook-Air.local>
2025-09-05 17:04:52 +0800

    Initial commit

--- Project4_BgChanger/App.tsx
+++ Project4_BgChanger/App.tsx
@@ -1,45 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {View, Text, StyleSheet} from "react-native"
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import React from "react"
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+const App = () =>
+{
+  return(
+    <View>
+      <Text>Hello and welcome to the 4th project, where you will learn changing the backgrounf and few other hacks of RN</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
-export default App;
+
+
+export default App
